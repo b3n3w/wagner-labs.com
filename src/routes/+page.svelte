ef73da19949dda80d319cbabba4eb844d184a4d4
Benedikt Wagner <benedikt.wagner@hoch-rein.com>
2024-09-01 19:27:47 +0200

    add tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,14 @@
 </script>
 
 <main>
-	<div class="container">
-		<div class="row text-center">
-			<div class="col-12">
-				<h1 class="text-h4 font-weight-thin">Build in progress</h1>
+	<div class="flex justify-center text-center items-center">
+		<div class="flex flex-col">
+			<div class="flex flex-col gap-5">
+				<h1 class="text-3xl uppercase font-light">Build in progress</h1>
 				<img alt="Wagner-Lab Logo" src={logo} />
-				<p class="font-weight-thin">BENEDIKT WAGNER</p>
+				<p class="text-xl font-thin">BENEDIKT WAGNER</p>
 			</div>
-			<div class="col">
+			<div class="flex flex-row justify-center mt-4">
 				<a href="https://www.linkedin.com/in/benedikt-wagner-808a101a9/" class="btn btn-icon">
 					<svg viewBox="0 0 24 24" width="36" height="36">
 						<path fill="currentColor" d={mdiLinkedin} />
@@ -31,41 +31,3 @@
 		</div>
 	</div>
 </main>
-
-<style>
-	.container {
-		height: 100vh;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-	.row {
-		width: 100%;
-	}
-	.text-center {
-		text-align: center;
-	}
-	.text-h4 {
-		font-size: 2.125rem;
-	}
-	.font-weight-thin {
-		font-weight: 100;
-	}
-	.my-3 {
-		margin-top: 1rem;
-		margin-bottom: 1rem;
-	}
-	img {
-		max-height: 200px;
-		width: auto;
-	}
-	.btn-icon {
-		display: inline-flex;
-		align-items: center;
-		justify-content: center;
-		padding: 0.5rem;
-		margin: 0 0.25rem;
-		color: inherit;
-		text-decoration: none;
-	}
-</style>
