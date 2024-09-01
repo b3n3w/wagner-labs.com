b883f199c90bee64aa20f56bb991f40dae1111a5
Benedikt Wagner <benedikt.wagner@hoch-rein.com>
2024-09-01 19:38:20 +0200

    ta

--- svelte.config.js
+++ svelte.config.js
@@ -9,11 +9,9 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'index.html',
 			precompress: false,
 			strict: true
 		})
